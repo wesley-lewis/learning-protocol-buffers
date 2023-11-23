bb57c62b554c9f43a2fc82da2e5c4c937a5183a5
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-23 23:58:53 +0530

    embedded data types

--- main.go
+++ main.go
@@ -8,29 +8,16 @@ import (
 )
 
 func main() {
-	fmt.Println("hello, world")
-	user := model.User{
-		UserId: "1",
-		Email: "wesley@gmail.com",
-	}
-	user2 := model.User{
-		UserId: "2",
-		Email: "hrushi@gmail.com",
-	}
-
 	group := model.Group{
 		Id: 1,
 		Category: model.Category_DEVELOPER,
-		Score: 2.4,
-		Users: []*model.User{&user, &user2},
+		Score: 23.3,
 	}
 
-	fmt.Println("To encode:", group.String())
+	fmt.Println(&group)
 
 	encoded, _ := proto.Marshal(&group)
-	
-	recovered := model.Group{}
-
-	_ = proto.Unmarshal(encoded, &recovered)
-	fmt.Println("Recovered:", recovered.String())
+	var grp model.Group
+	proto.Unmarshal(encoded, &grp)
+	fmt.Println(&grp)
 }
