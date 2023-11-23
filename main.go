d3c1c811933f6ad53d12fa13e52c8fe6f1a098d6
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-24 00:25:14 +0530

    created skeleton for further implementation

--- main.go
+++ main.go
@@ -1,17 +1,28 @@
+// Title: GRPC
+
 package main
 
 import (
 	"fmt"
 
+	"context"
+
 	"github.com/wesley-lewis/learning-proto-buf/model"
+	"google.golang.org/grpc"
 	"google.golang.org/protobuf/proto"
 )
 
 func main() {
+	userA := model.Group_User {
+		UserId: "Wesley",
+		Email: "wesley@gmail.com",
+	}
+	
 	group := model.Group{
 		Id: 1,
 		Category: model.Category_DEVELOPER,
 		Score: 23.3,
+		Users: []*model.Group_User{&userA},
 	}
 
 	fmt.Println(&group)
@@ -20,4 +31,24 @@ func main() {
 	var grp model.Group
 	proto.Unmarshal(encoded, &grp)
 	fmt.Println(&grp)
+}
+
+type UserServiceClient interface {
+	GetUser(ctx context.Context, in *model.UserRequest, opts grpc.CallOption) (*model.User, error)
+}
+
+type userServiceClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewUserServiceClient(cc grpc.ClientConnInterface) UserServiceClient {
+	return &userServiceClient{cc}
+}
+
+func(c *userServiceClient) GetUser(ctx context.Context, in *model.UserRequest, opts grpc.CallOption) (*model.User, error) {
+	return nil, nil
+}
+
+type UserServiceServer interface {
+	GetUser(ctx context.Context, in *model.UserRequest, opts grpc.CallOption) (*model.User, error)
 }
